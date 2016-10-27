9d4590ec205cd7ece3b80b347c89a3786c70ca8a
Lalit Solanki <Lalit Solanki>
2016-10-27 00:53:55 -0400

    Adding burgers

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,15 @@
-// Making an Express server available
-var express = require('express');
-var app = express();
-
-// Adding in the components of the mysql library. Outlining the parameters tied to the mysql db
+/*
+Here is where you make the connection to the database and export and used by the O.R.M.
+*/
 var mysql = require('mysql');
 var connection = mysql.createConnection({
+	port: 3306,
 	host: 'localhost',
 	user: 'root',
 	password: 'root',
 	database: 'burgers_db'
 });
 
-// Connecting the mysql database mentioned above
 connection.connect(function (err) {
 	if (err) {
 		console.error('error connecting: ' + err.stack);
@@ -19,3 +17,5 @@ connection.connect(function (err) {
 	}
 	console.log('connected as id ' + connection.threadId);
 });
+
+module.exports = connection;
